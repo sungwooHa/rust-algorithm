5e0d7b379b5656956f35993ad26985fb8f81391c
ttlhi10 <ttlhi10@gmail.com>
2024-01-18 01:11:30 +0900

    problem10171, 10172, "//"

--- src/sprout/output.rs
+++ src/sprout/output.rs
@@ -1,8 +1,4 @@
 
-pub trait Problem {
-    fn solve();
-}
-
 pub mod problem2557 {
     pub fn problem() {
         println!("Hello World!");
@@ -13,8 +9,57 @@ pub mod problem2557 {
 pub mod problem10699
 {
     use chrono::prelude::*;
-    pub fn problem(){
+
+    pub fn problem() {
+        problemByChrono();
+        get_current_date();
+    }
+
+    pub fn problemByChrono(){
         let local_time = Local::now();
-        print!("{}", local_time.format("%Y-%m-%d").to_string())
+        println!("{}", local_time.format("%Y-%m-%d").to_string());
+    }
+
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    pub fn get_current_date() {
+        let start = SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let in_days = since_the_epoch.as_secs() / 86400;
+        
+        let (is_common_era, year) = if in_days >= 719468 {
+            (true, (in_days - 719468) / 36524)
+        } else {
+            // Check if in_days is greater than 719468 before subtracting
+            if 719468 > in_days {
+                (false, (719468 - in_days) / 36524)
+            } else {
+                (false, 0)
+            }
+        };
+        
+        // let year : i64 = if is_common_era { (year + 1).try_into().unwrap() } else { (1 - year).try_into().unwrap()};
+        // println!("{}-01-01", year);
+    }
+}
+
+pub mod problem10171{
+    pub fn problem() {
+        println!("\\    /\\");
+        println!(" )  ( ')");
+        println!("(  /  )");
+        println!(" \\(__)|");
+    }
+}
+
+pub mod problem10172{
+    pub fn problem() {
+        println!("|\\_/|");
+        println!("|q p|   /}");
+        println!("( 0 )\"\"\"\\");
+        println!("|\"^\"`    |");
+        println!("||_/=\\\\__|");
     }
 }
