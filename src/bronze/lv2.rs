a31b1e953405cc0107c6543dbb81fc517a888550
ttlhi10 <ttlhi10@gmail.com>
2024-02-05 21:39:14 +0900

    5364 최적화(min_by , total_cmp )

--- src/bronze/lv2.rs
+++ src/bronze/lv2.rs
@@ -3,9 +3,8 @@ use std::result;
 pub fn problem5364() {
     //평면상의 거리 찾기.
     //기준 (X,Y)로 부터 가장 가까운 거리를 찾는다.
-    //거리 = (x1-x)^2 + (y1-y)^2
+    //거리 = ((x1-x)^2 + (y1-y)^2).sqrt()
     //소수점 2자리까지 출력
-
     fn distance(cp: (i32, i32), dest: (i32, i32)) -> f32 {
         let (x, y) = cp;
         let (x1, y1) = dest;
@@ -28,17 +27,19 @@ pub fn problem5364() {
 
     let cp = get_pt_by_input();
 
-    println!("{:?}, {:?}", cp, number_planet);
     let mut plants_with_dist: Vec<((i32, i32), f32)> = Vec::new();
     for _ in 0..number_planet - 1 {
         let plant = get_pt_by_input();
         let dist = distance(cp, plant);
         plants_with_dist.push((plant, dist));
     }
-
-    plants_with_dist.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    
+    let res = plants_with_dist.iter().min_by(|a, b| a.1.total_cmp(&b.1));
+    //plants_with_dist.sort_by(|a, b| a.1.total_cmp(&b.1).unwrap());
 
     println!("{} {}", cp.0, cp.1);
-    println!("{} {}", plants_with_dist[0].0 .0, plants_with_dist[0].0 .1);
-    println!("{}", plants_with_dist[0].1);
+    println!("{} {}", res.unwrap().0.0, res.unwrap().0.1);
+    println!("{}", res.unwrap().1);
+    //println!("{} {}", plants_with_dist[0].0 .0, plants_with_dist[0].0 .1);
+    //println!("{}", plants_with_dist[0].1);
 }
